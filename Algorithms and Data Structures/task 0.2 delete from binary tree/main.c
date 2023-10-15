7890535eda2257eae8640331841227fc359309ac
MavikBow <mrmavikd@gmail.com>
2023-10-15 18:05:07 +0300

    started doing AaDS/task 0.2 deleting from binary tree

---
+++ Algorithms and Data Structures/task 0.2 delete from binary tree/main.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int target;
+
+struct node
+{
+    int key;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int value)
+{
+    struct node* newNode = (struct node *) malloc(sizeof(struct node));
+
+    newNode->key = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void addNode(struct node* root, int value)
+{
+    if(value == root->key)
+    {
+        return;
+    }
+    
+    if(value < root->key)
+    {
+        if(root->left == NULL)
+        {
+            root->left = createNode(value);
+            return;
+        }
+        else
+        {
+            addNode(root->left, value);
+            return;
+        }
+    }
+    else
+    {
+        if(root->right == NULL)
+        {
+            root->right = createNode(value);
+            return;
+        }
+        else
+        {
+            addNode(root->right, value);
+            return;
+        }
+    }
+
+    return;
+}
+
+void memFree(struct node** root)
+{
+    if((*root)->left != NULL)
+    {
+        memFree(&(*root)->left);
+    }
+
+    if((*root)->right != NULL)
+    {
+        memFree(&(*root)->right);
+    }
+
+    free(*root);
+    *root = NULL;
+}
+
+void straightLeft_fprintf(FILE* out, struct node* root)
+{
+    if(root != NULL)
+    {
+        fprintf(out, "%d\n", root->key);
+        straightLeft_fprintf(out, root->left);
+        straightLeft_fprintf(out, root->right);
+    }
+}
+
+void readInputs(struct node** root)
+{
+    FILE *in = fopen("input.txt", "r");
+    if (in == NULL)
+    {
+        printf("error opening input.txt\n");
+        exit(1);
+    }
+
+    int root_value, leaf_value;
+
+    fscanf(in, "%d\n\n", &target);
+    fscanf(in, "%d\n", &root_value);
+
+    *root = createNode(root_value);
+
+    while(!feof(in))
+    {
+        fscanf(in, "%d\n", &leaf_value);
+        addNode(*root, leaf_value);
+    }
+
+    fclose(in);
+}
+
+void writeOutputs(struct node* root)
+{
+    FILE* out = fopen("output.txt", "w");
+    if (out == NULL)
+    {
+        printf("error opening output.txt\n");
+        exit(1);
+    }
+
+    straightLeft_fprintf(out, root);
+
+    fclose(out);
+}
+
+int leftmost_key(struct node** root)
+{
+    if((*root)->left != NULL)
+    {
+        return leftmost_key(&(*root)->left);
+    }
+
+    return (*root)->key;
+}
+
+void straightLeft_search(struct node** root, int victim)
+{
+    if(*root == NULL){
+        return;
+    }
+
+    if((*root)->key == victim)
+    {
+        if((*root)->left == NULL && (*root)->right == NULL)
+        {
+            // Delete the node
+
+            free(*root);
+            *root = NULL;
+            return;
+        }
+
+        if((*root)->left != NULL && (*root)->right == NULL)
+        {
+            // Merge the tree
+
+            struct node* temp = (*root)->left;
+
+            (*root)->key = (*root)->left->key;
+            (*root)->right = (*root)->left->right;
+            (*root)->left = (*root)->left->left;
+
+            free(temp);
+            return;
+        }
+
+        if((*root)->right != NULL)
+        {
+            int new_victim = leftmost_key(&(*root)->right); 
+            (*root)->key = new_victim;
+
+            straightLeft_search(&(*root)->right, new_victim);
+
+            return;
+        }
+
+        return;
+
+    }
+
+    if((*root)->key > victim)
+        straightLeft_search(&(*root)->left, victim);
+    if((*root)->key < victim)
+        straightLeft_search(&(*root)->right, victim);
+}
+
+int main()
+{
+    struct node* root;
+
+    readInputs(&root);
+
+    straightLeft_search(&root, target);
+        
+    writeOutputs(root);
+
+    memFree(&root);
+
+    return 0;
+}
